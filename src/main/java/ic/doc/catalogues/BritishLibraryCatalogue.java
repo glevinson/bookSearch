f55c50249b511ac43777b578f3a713125397b8bc
katie <katiemiles12345@gmail.com>
2022-02-22 14:51:53 +0000

    Katie&Gus: created an interface Library which has a searchFor function which can allow use of different libraries

--- src/main/java/ic/doc/catalogues/BritishLibraryCatalogue.java
+++ src/main/java/ic/doc/catalogues/BritishLibraryCatalogue.java
@@ -7,12 +7,13 @@ import static ic.doc.catalogues.QueryParser.publishedBeforeFrom;
 import static ic.doc.catalogues.QueryParser.titleFrom;
 
 import ic.doc.Book;
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class BritishLibraryCatalogue {
+public class BritishLibraryCatalogue implements Library{
 
   // imagine that each new instance of this object uses more than 500MB of RAM
 
@@ -27,6 +28,7 @@ public class BritishLibraryCatalogue {
     return instance;
   }
 
+  @Override
   public List<Book> searchFor(String query) {
     return catalogue.stream()
         .filter(book -> book.matchesAuthor(lastNameFrom(query)))
