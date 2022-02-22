ff5aef8c668c6ca411c5442344c433d94c0fc198
katie <katiemiles12345@gmail.com>
2022-02-22 14:32:55 +0000

    Katie&Gus: created QueryBuilder and separated out query parameters. Can create object with forname, surname, title, published after date, or published before date. Fields initially set to null.

---
+++ src/main/java/ic/doc/QueryBuilder.java
@@ -0,0 +1,40 @@
+package ic.doc;
+
+public class QueryBuilder {
+
+  private String name1 = null;
+  private String name2 = null;
+  private String title = null;
+  private Integer date1 = null;
+  private Integer date2 = null;
+
+  public QueryBuilder withForename(String name){
+    this.name1 = name;
+    return this;
+  }
+
+  public QueryBuilder withSurname(String name){
+    this.name2 = name;
+    return this;
+  }
+
+  public QueryBuilder withTitle(String title){
+    this.title = title;
+    return this;
+  }
+
+  public QueryBuilder withPublishedAfter(int date){
+    this.date1 = date;
+    return this;
+  }
+
+  public QueryBuilder withPublishedBefore(int date){
+    this.date2 = date;
+    return this;
+  }
+
+  public BookSearchQuery build(){
+    new BookSearchQuery(name1, name2, title, date1, date2);
+  }
+
+}
